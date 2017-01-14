54050dfff0db77ac76ea89914e9a576ce126d3ee
Krynasty <daniel-pb@hotmail.co.uk>
2017-01-14 07:22:21 +0000

    Password Managment Application
    storage of passwords as well as creation of new passwords. the ability
    to encrypt passwords so that only login users can access.

---
+++ PasswordManager/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PasswordManager
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button main;
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.logoLabel = new System.Windows.Forms.Label();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            main = new System.Windows.Forms.Button();
+            this.sidePanel.SuspendLayout();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // main
+            // 
+            main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            main.Dock = System.Windows.Forms.DockStyle.Top;
+            main.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            main.FlatAppearance.BorderSize = 0;
+            main.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            main.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            main.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            main.Location = new System.Drawing.Point(0, 0);
+            main.Name = "main";
+            main.Size = new System.Drawing.Size(239, 70);
+            main.TabIndex = 1;
+            main.Text = "         Main Page";
+            main.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            main.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "         Generate";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(0, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 70);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "         Sign In";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // sidePanel
+            // 
+            this.sidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(14)))));
+            this.sidePanel.Controls.Add(this.button3);
+            this.sidePanel.Controls.Add(this.button2);
+            this.sidePanel.Controls.Add(this.button1);
+            this.sidePanel.Controls.Add(main);
+            this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidePanel.Location = new System.Drawing.Point(0, 58);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(239, 503);
+            this.sidePanel.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(0, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 70);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "         Help";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseMnemonic = false;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // logoLabel
+            // 
+            this.logoLabel.AutoSize = true;
+            this.logoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoLabel.ForeColor = System.Drawing.Color.White;
+            this.logoLabel.Location = new System.Drawing.Point(35, 18);
+            this.logoLabel.Name = "logoLabel";
+            this.logoLabel.Size = new System.Drawing.Size(122, 25);
+            this.logoLabel.TabIndex = 0;
+            this.logoLabel.Text = "Baton Pass";
+            this.logoLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.topPanel.Controls.Add(this.label2);
+            this.topPanel.Controls.Add(this.label1);
+            this.topPanel.Controls.Add(this.logoLabel);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1248, 58);
+            this.topPanel.TabIndex = 1;
+            this.topPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.topPanel_Paint);
+            this.topPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseDown);
+            this.topPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseMove);
+            this.topPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1189, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "-";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1219, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseDown);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseHover += new System.EventHandler(this.label1_MouseHover);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(27)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(1248, 561);
+            this.Controls.Add(this.sidePanel);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.sidePanel.ResumeLayout(false);
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel sidePanel;
+        private System.Windows.Forms.Label logoLabel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PasswordManager/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PasswordManager
+{
+    public partial class Home : Form
+    {
+        int moveScreen;
+        int mPosX;
+        int mPosY;
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void topPanel_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void label1_Click_1(object sender, EventArgs e) // Close the program
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e) // Minimise the program
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void topPanel_MouseDown(object sender, MouseEventArgs e)   //whilst the mouse is held down, get the positions   
+        {
+             moveScreen = 1;
+             mPosX = e.X;
+             mPosY = e.Y;
+        }
+
+        private void topPanel_MouseUp(object sender, MouseEventArgs e)  //  static screen, this is a non-moving state. 
+        {
+            moveScreen = 0;
+
+        }
+
+        private void topPanel_MouseMove(object sender, MouseEventArgs e)    //  moving the screen and updating the position on the screen.
+        {
+            if (moveScreen == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - mPosX, MousePosition.Y - mPosY);
+            }
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.Black;
+            label2.BackColor = Color.White;
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.White;
+            label2.BackColor = Color.DodgerBlue;
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Black;
+            label1.BackColor = Color.White;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.White;
+            label1.BackColor = Color.DodgerBlue;
+        }
+
+    }
+}
